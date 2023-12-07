f59aa1cb0c54aa0a8bcffc7e90f4d8247e2480a1
ADizzyPython <64909375+ADizzyPython@users.noreply.github.com>
2023-12-07 06:54:24 +0530

    Create HeroSection.js

---
+++ HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HeroSection() {
+  return (
+    <section className="hero-section">
+      <h1>Unwind and spin your stress away</h1>
+      <img src="./assets/spinner1.jpg" alt="Spinnr keychain spinner in use" />
+      <a href="#products" className="btn btn-primary">Shop Now</a>
+    </section>
+  );
+}
+
+export default HeroSection;
