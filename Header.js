e12d7b81be57e51dd04514317ffdd0c3743ad889
ADizzyPython <64909375+ADizzyPython@users.noreply.github.com>
2023-12-07 06:54:07 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <img src="./assets/logo.png" alt="Spinnr logo" />
+    </header>
+  );
+}
+
+export default Header;
